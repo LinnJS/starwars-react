5d909a52f2298c1cb21f0bc2461b7360cdb9317b
JSLinn727 <justinslinn727@gmail.com>
2017-08-17 13:52:00 -0400

    Initial commit

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,13 @@ class App extends Component {
   // Set props and state below.
   // Set state for vehicles (empty array), value (empty string), pilot (empty) string.
 
-
   // FORM: HANDLE INPUT CHANGES
   // handleNameChange below:
-
 
   //  FORM: SUBMIT METHOD
   // handleSubmit below:
   // Once the form is sumbited, two things need to happen: set the state of pilot to the input value.
   // Then, set the value of the input back to an empty string.
-
 
   // LIFECYCLE
   // Which lifecycle is best for fetching data?
@@ -23,7 +20,6 @@ class App extends Component {
   // Once data fetched, set the state of vehicles to the fetched data.
   // In response look for 'results'. It should return this array.
   // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
-
 
   // RENDER
   // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
@@ -37,17 +33,17 @@ class App extends Component {
     Store vehicles state in a variable.
     Map over this variable to access the values needed to render.
     */
-    })
-    return (
-      <div className="App">
-        {/*
+  })
+  return (
+    <div className="App">
+      {/*
         The App component needs the following:
          jumbotron section, form section, vehicle cards section.
          Form will also need a header in which you will pass the state of the form upon submit.
          */}
-      </div>
-    );
-  }
+    </div>
+  );
+}
 }
 
 export default App;
