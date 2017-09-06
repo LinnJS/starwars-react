1b7bf410670517cce36a98a779b53ff86c052a44
JSLinn727 <justinslinn727@gmail.com>
2017-09-06 14:46:36 -0400

    Fixed jumbotron

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -23,7 +23,7 @@ class Form extends Component {
       <div className="jumbotron icon">
         <img src={rebel} alt="Rebel Icon" />
         <h2>May the Force be with you</h2>
-        <img src={empireß} alt="Rebel Icon" />
+        <img src={empire} alt="Rebel Icon" />
       </div>
     )
   }
